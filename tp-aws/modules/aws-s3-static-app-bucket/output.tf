a51ef975f872cf05f5e3ef5483872b112bbe513e
s LOUNNAS <miyohar915@tagbert.com>
2022-06-27 15:38:39 +0200

    add first resource

--- tp-aws/modules/aws-s3-static-app-bucket/output.tf
+++
@@ -1,5 +0,0 @@
-// displaying name of buckets
-output "name" {
-    description = "Name nof the bucket"
-    value       = aws_s3_bucket.bucket.id
-}
--- tp-aws/output.tf
+++
@@ -1,3 +0,0 @@
-output "adresse_ip_instance" {
-  value = aws_instance.tp_ec2_instance.public_ip
-}
